1c7887c7d8206f945db6ffa4e97d035d8136384a
10293876 <kwalz888@gmail.com>
2024-08-28 19:52:19 -0400

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -4496,6 +4496,7 @@ var Incremancer;
                 d = Math.abs(h),
                 c = Math.abs(l);
             let u = 1 / Math.max(d, c);
+            u *= 1.29289 - (d + c) * u * .29289, o.xSpeed = h * u * this.speed, o.ySpeed = 1 * u * this.speed, o.rotation = Math.atan2(o.ySpeed, o.xSpeed)
             o.update = () => {
             if (!o.target) return;
                 const dx = o.target.x - o.x;
