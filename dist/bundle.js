fb488f6678a3ad2bb66287b24041388bface9a47
10293876 <kwalz888@gmail.com>
2024-08-29 19:55:26 -0400

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -2644,9 +2644,34 @@ var Incremancer;
         }
         updateDeadHumanFading(e, t) {
             if (e.visible) {
-                if (e.alpha > .5 && e.alpha - this.fadeSpeed * t <= .5 && !e.flags.tank && Math.random() < this.gameModel.riseFromTheDeadChance) return this.zombies.createZombie(e.x, e.y, e.flags.dog), e.visible = !1, void g.removeChild(e);
-                e.alpha -= this.fadeSpeed * t, e.alpha < 0 && (e.visible = !1, g.removeChild(e)), (e.alpha > .5 && e.alpha - this.fadeSpeed * t <= .5 && !e.flags.tank && Math.random() < this.gameModel.biomassrisefromdead) return this.biomasses.createBiomass(e.x, e.y), e.visible = !1, void g.removeChild(e);
-                e.alpha -= this.fadeSpeed * t, e.alpha < 0 && (e.visible = !1, g.removeChild(e))
+                if (e.alpha > 0.5) {
+                    e.alpha -= this.fadeSpeed * t;
+                }
+            
+                let zombieSpawned = false;
+                let biomassSpawned = false;
+            
+                // Check for spawning a zombie
+                if (e.alpha <= 0.5 && e.flags.tank && Math.random() < this.gameModel.riseFromTheDeadChance) {
+                    this.zombies.createZombie(e.x, e.y, e.flags.dog);
+                    zombieSpawned = true;
+                }
+            
+                // Check for spawning a biomass
+                if (e.alpha <= 0.5 && e.flags.tank && Math.random() < this.gameModel.biomassrisefromdead) {
+                    this.biomasses.createBiomass(e.x, e.y, e.flags.dog);
+                    biomassSpawned = true;
+                }
+            
+                // Remove the entity if the alpha is fully faded out
+                if (e.alpha <= 0) {
+                    e.visible = false;
+                    g.removeChild(e);
+                } else if (zombieSpawned || biomassSpawned) {
+                    // Also remove the entity if either was spawned
+                    e.visible = false;
+                    g.removeChild(e);
+                }
             }
         }
         changeState(e, t) {
