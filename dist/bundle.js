612b76a554f946bced680a49367ded29dd107695
10293876 <kwalz888@gmail.com>
2024-08-30 15:03:24 -0400

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -3175,17 +3175,23 @@ var Incremancer;
         }
         populate() {
             if (this.map = new ee, this.model = ne.getInstance(), this.humans = new Se, this.graveyard = new Oe, this.creatureFactory = new ae, this.smoke = new ot, this.blood = new _e, this.bones = new tt, this.exclamations = new it, this.blasts = new nt, this.bullets = new rt, this.model.zombieCount = 0, 0 == this.textures.length) {
-                for (let e = 0; e < 3; e++) {
+                for (let e = 0; e < 3; e++){
                     const t = [];
                     const j = [];
-                    for (let s = 0; s < 3; s++) t.push(PIXI.Texture.from("zombie" + (e + 1) + "_" + (s + 1) + ".png"));
-                    for (let u = 0; u < 3; u++) j.push(PIXI.Texture.from("biomass" + (e + 1) + "_" + (u + 1) + ".png"));
+                    for (let e = 0; e < 3; e++) {
+                        t.push(PIXI.Texture.from("zombie" + (e + 1) + "-" + (s + 1) + ".png"));
+                    }
                     this.textures.push({
                         animated: t,
-                        dead: [PIXI.Texture.from("zombie" + (e + 1) + "_dead.png")],
-                        animated: j,
+                        dead: [PIXI.Texture.from("zombie" + (e + 1) + "_dead.png")]
+                    });
+                for (let e = 0; e < 3; e++) {
+                        u.push(PIXI.Texture.from("biomass" + (e + 1) + "-" + (u + 1) + ".png"));
+                    }
+                    this.textures.push({
+                        animated: u,
                         dead: [PIXI.Texture.from("biomass" + (e + 1) + "_dead.png")]
-                    })
+                    });
                 }
                 for (let e = 0; e < 2; e++) this.dogTexture.push(PIXI.Texture.from("zombiedog" + (e + 1) + ".png"));
                 this.deadDogTexture = [PIXI.Texture.from("zombiedogdead.png")]
